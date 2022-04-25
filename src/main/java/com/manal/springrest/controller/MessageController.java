6f30da9d35d074c9d4b3a0d09dee141080abee75
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-04-25 18:51:34 +0200

    Sending data

--- src/main/java/com/manal/springrest/controller/MessageController.java
+++ src/main/java/com/manal/springrest/controller/MessageController.java
@@ -3,19 +3,24 @@ package com.manal.springrest.controller;
 import com.manal.springrest.model.Message;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MessageController {
 
   //this tells Spring that we are returning a response body
   //and spring will convert our message object into a
   //JSON message
-  @ResponseBody
   @GetMapping("/message")
   //to return a JSON message with the content we need a model class for that
   Message send() {
-    return new Message("first message");
+    return new Message("first message: Get");
   }
 
+  @PostMapping("/message")
+  Message echo(@RequestBody Message message) {
+    return message;
+  }
 }
